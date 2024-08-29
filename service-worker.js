8d546ee351eb026dbdb20c973244b6e95af6cf7d
Asif <iaasifmhd@gmail.com>
2024-08-29 12:49:39 +0530

    weatherapp_pwa

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'weather-app-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
